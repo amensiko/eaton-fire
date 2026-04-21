dd48b729f4fb2c94f0db818c476036ea084b5b02
elagerqu <erik.lagerquist@uvm.edu>
2026-04-21 13:38:14 -0400

    Implement load_aqs function for AQ station data
    Add function to load and process AQ station data from CSV.

--- merge_data_sources.py
+++ merge_data_sources.py
@@ -5,3 +5,18 @@
 
 
 #AQ station should probably be selected and/or averaged here, with 
+
+def load_aqs():
+  aqs_csv = r"pm25_focal_stations_daily.csv"
+  df = pd.read_csv(aqs_csv, parse_dates=["date"]) #read csv
+
+  #create column for each station
+  df_aqs = (
+      df.pivot(index="date", columns="station", values="pm25")
+      .rename(columns=lambda s: f"aqs_{s}_pm25")
+      .reset_index()
+  )
+  df_aqs.columns.name = None
+  
+  #create averae column
+  df_aqs["aqs_avg_pm25"] = df_wide.filter(like="aqs_").mean(axis=1)
