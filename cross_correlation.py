67f573852f7518491a398ee9bfeedd3a0d3b6240
Will Behm <willbehm@Wills-MacBook-Pro.local>
2026-04-21 11:56:23 -0400

    added 95% confidence intervals

--- cross_correlation.py
+++ cross_correlation.py
@@ -7,7 +7,7 @@ def load_wx():
     df = pd.read_csv(wx_daily)
     print(df.head)
     #drop extraneous
-    df.drop(columns=["Year", "Month", "Day"])
+    df = df.drop(columns=["Year", "Month", "Day"])
     #Convert to datetime
     df["DateString"] = pd.to_datetime(df["DateString"])
 
@@ -19,7 +19,8 @@ def cross_correlation(df):
     cross_correlation = ccf(x=df["ppt"], #x shifts in time, ie, for lag k, the data is shifted to start at x(t+k)
                             y=df["vpdmax"], #y stays in place
                             nlags=25, #More than this and things start to get unreliable (less data) - are there domain reasons to go beyond?
-                            adjusted=True
+                            adjusted=True,
+                            alpha=0.05
                             )
     print(cross_correlation)
     
@@ -27,11 +28,23 @@ def cross_correlation(df):
 
 def plot_correlations(cross_corr):
 
+    correlations = cross_corr[0]
+
+    confidence_ints = cross_corr[1]
+
     #pull out the lag number (just the index of the correlation)
-    lags = [i for i in range(len(cross_corr))]
+    lags = [i for i in range(len(correlations))]
+
     
     #plot with lags as x, correlations as y:
-    fig = go.Figure(go.Bar(x=lags, y=cross_corr))
+    fig = go.Figure(go.Bar(x=lags, 
+                           y=correlations, 
+                           error_y=dict(
+                            type='data',
+                            symmetric=False,
+                            array=confidence_ints[:, 1] - correlations,
+                            arrayminus=correlations - confidence_ints[:, 0])
+                            ))
 
     fig.update_yaxes(title_text = "Correlation")
     fig.update_xaxes(title_text = "Lag (days)")
