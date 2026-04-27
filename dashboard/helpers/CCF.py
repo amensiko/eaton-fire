36913dbec6124144dd93b0fb86f2ba780695c8da
Will Behm <willbehm@ip0af53d8c.int.uvm.edu>
2026-04-27 12:33:29 -0400

    Dashboard update for Cross Correlation

---
+++ dashboard/helpers/CCF.py
@@ -0,0 +1,110 @@
+from statsmodels.tsa.stattools import ccf
+import pandas as pd
+import plotly.graph_objects as go
+from CCF_utils import merge_sources, load_variable_lookup
+
+#One function to calculate, one function to execute
+def compute_cross_correlation(df, var1, var2):
+    """
+    Computes temporal cross correlations and 95% confidence intervals for two variables
+
+    Parameters: 
+
+    df (pd.dataframe): dataframe with daily data for all variables of interest
+    var1 (str): "driver" variable for cross correlation - must exist in df
+    var2 (str): "lagged" variable for cross correlation - must exist in df
+    
+    """
+
+    cross_correlation = ccf(x=df[var1], #x shifts in time, ie, for lag k, the data is shifted to start at x(t+k)
+                            y=df[var2], #y stays in place
+                            nlags=25, #More than this and things start to get unreliable (less data) - are there domain reasons to go beyond?
+                            adjusted=True, # adjust for decreasing number of samples at higher lags
+                            alpha=0.05 #95% confidence interval
+                            )
+    
+    return cross_correlation
+
+def plot_correlations(cross_corr, var1, var2):
+
+    """
+    Plots the cross-correlations
+    """
+
+    correlations = cross_corr[0]
+
+    confidence_ints = cross_corr[1]
+
+    #pull out the lag number (just the index of the correlation)
+    lags = [i for i in range(len(correlations))]
+
+    #plot with lags as x, correlations as y:
+    fig = go.Figure(go.Bar(x=lags, 
+                           y=correlations, 
+                           error_y=dict(
+                            type='data',
+                            symmetric=False,
+                            array=confidence_ints[:, 1] - correlations,
+                            arrayminus=correlations - confidence_ints[:, 0])
+                            ))
+    
+    var_lookup = load_variable_lookup()
+    
+    #Get Variable Labels
+    var1_label = var_lookup[var1]
+    var2_label = var_lookup[var2]
+    
+
+    fig.update_layout(title=f"Cross-Correlation Plot: \n{var1_label} & {var2_label}")
+    fig.update_yaxes(title_text = "Correlation")
+    fig.update_xaxes(title_text = "Lag (days)")
+
+    fig.show()
+
+def cross_correlation(var1, var2, station=None):
+    """ 
+    This function computes cross correlation and makes a plot for a given set of variables, and a selected weather station if applicable.  
+
+    var1 (str): x/driver variable for CCF
+    var2 (str): y/lagged variable for CCF
+    station(str): weather station for PM25 data (optional - takes average if not specified)
+    """
+    #merge processed data across different sources
+    df = merge_sources(station)
+
+    #Calculate cross correlations
+    cross_corr = compute_cross_correlation(df, var1, var2)
+
+    #Plot it
+    plot_correlations(cross_corr, var1, var2)
+ 
+
+
+def main():
+    """
+    selection of variables and weather station
+    """
+
+    #Variables to run CCF on - see variables.yaml
+    var1 = "ppt"
+    var2 = "pm25"
+
+    #Stations to select from - right now you HAVE to choose a station
+    stations = ["Glendora",
+                "Los Angeles-North Main Street",
+                "North Hollywood (NOHO)",
+                "Pasadena"
+                ]
+    
+    #select station
+    station = stations[2]
+
+    cross_correlation(var1="ppt", #Select x (driver) variable
+                      var2="pm25", #Select y (lagged) variable
+                      station=None #select station for PM25 (or not - will average)
+                      )
+    
+
+
+if __name__ == "__main__":
+    main()
---
+++ dashboard/helpers/CCF_utils.py
@@ -0,0 +1,99 @@
+import pandas as pd
+from functools import reduce
+import yaml
+#Utils to merge data sources from biodiversity observation, weather, news article, and air quality data.
+
+
+def load_wx():
+    wx_daily = r"data/weather/daily_data.csv"
+    df = pd.read_csv(wx_daily)
+    
+    
+    #Convert to datetime
+    df["date"] = pd.to_datetime(df["DateString"], format="%Y%m%d")
+
+    #drop extraneous
+    df = df.drop(columns=["Year", "Month", "Day", "DateString"])
+    print(df.head)
+    return df
+
+def load_aq(station=None):
+    """
+    loads air quality data, subsetted by station
+    """
+    #load processed data
+    pm25_daily = "data/airquality/pm25_focal_stations_daily.csv"
+    df = pd.read_csv(pm25_daily)
+    #manage datetime
+    df["date"] = pd.to_datetime(df['date'], format="%Y-%m-%d")
+
+    #subset to station if requested, otherwise take the average across stations
+    if station:
+        #subset by station
+        df = df[df['station']==station]
+        df= df.drop(columns=["station", "label"]) #drop station column (filtered in function)
+    else:
+        df= df.groupby("date").mean(numeric_only=True).reset_index()
+        df= df.drop(columns=["interpolated"]) #drop station column (filtered in function)
+
+    return df
+
+def load_biodiversity():
+
+    bio_daily = r"data/biodiversity/biodiversity_daily_counts_cr.csv"
+    df = pd.read_csv(bio_daily)
+    
+    
+    #Convert to datetime
+    df["date"] = pd.to_datetime(df["day"], format="%Y-%m-%d")
+
+    df = df.drop(columns=["day"])
+
+
+    return df
+
+def load_news():
+    news_daily = r"data/news/news_daily_counts_cr.csv"
+    df = pd.read_csv(news_daily)
+    #Convert to datetime
+    df["date"] = pd.to_datetime(df["publish_date"], format="%Y-%m-%d")
+    df = df.drop(columns=["publish_date"])
+
+
+def merge_sources(station=None):
+    """
+    Merges air quality (subsetted by station), weather data sources (to add, biodiversity and news articles) - assumed to be on a daily timestep from 01/25 to 03/25
+    """
+    #initialize list to hold loaded data
+    to_merge = []
+
+    #load weather
+    wx_data = load_wx()
+    to_merge.append(wx_data)
+
+    #load pm25
+    pm25_data = load_aq(station) #load
+    to_merge.append(pm25_data)
+
+    bio_data = load_biodiversity()
+    to_merge.append(bio_data)
+    
+    merged = reduce(lambda left, right: left.merge(right, on="date"), to_merge)
+
+    return merged
+
+def load_variable_lookup():
+    var_yaml = r"data/variables.yaml"
+    var_lookup_flat = {}
+    
+    with open(var_yaml, 'r') as f:
+        var_lookup = yaml.safe_load(f)
+
+    for var_type, dict in var_lookup['variables'].items():
+        for key, val in dict.items():
+            var_lookup_flat[key] = val
+    
+    return var_lookup_flat
+
+#DateString can be kept, and year/month/day columns dropped for bio data.
+
